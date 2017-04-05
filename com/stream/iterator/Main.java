9ff462c0b821ac59d4d447ed8122cbdc5d778d5a
zhangchuanchuan <zhangchuanchuan@58ganji.com>
2017-04-05 22:00:24 +0800

    add note

--- com/stream/iterator/Main.java
+++ com/stream/iterator/Main.java
@@ -1,3 +1,8 @@
+/**
+*Aggregate是一个集合接口，容器一般都要实现这个接口，Iterator是迭代器接口，使用它遍历容器中的内容
+*Aggregate与Iterator的关系就是关联关系，Aggregate会创建一个Iterator。
+*BookShelf实现Aggregate的接口，BookShelfIterator实现Iterator接口
+*/
 public class Main {
   public static void main(String[] args) {
     BookShelf bookShelf = new BookShelf(4);
